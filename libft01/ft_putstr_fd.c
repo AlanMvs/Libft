114a936deaf2248cea9d281cb563d61b18040ef7
Alan Musaev <amusaev@c1r5s6.42prague.com>
2024-01-23 17:34:47 +0100

    Added ft_putchar and ft_putstr

---
+++ libft01/ft_putstr_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amusaev <amusaev@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 17:28:43 by amusaev           #+#    #+#             */
+/*   Updated: 2024/01/23 17:30:30 by amusaev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (s == NULL)
+		return ;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+// int main(void)
+// {
+//     char str[] = "hello dude";
+//     ft_putstr_fd(str,1);
+//     return (0);
+// }
