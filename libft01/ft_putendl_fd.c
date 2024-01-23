dbaa95a7d5961e51df387a47f5a18adc8b17b980
Alan Musaev <amusaev@c1r5s6.42prague.com>
2024-01-23 17:44:56 +0100

    Added ft_putendl and ft_putnbr

---
+++ libft01/ft_putendl_fd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amusaev <amusaev@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 17:39:06 by amusaev           #+#    #+#             */
+/*   Updated: 2024/01/23 17:39:28 by amusaev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (s == NULL)
+		return ;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+
+// int main(void)
+// {
+//     char str[] = "hello dude";
+//     ft_putendl_fd(str,1);
+//     return (0);
+// }
